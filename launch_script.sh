ef3b7ff3a990d6f583b54b49ba4cceadb5c94db2
woland7 <antonio.deiasio@gmail.com>
2021-03-04 12:19:52 +0100

    merged kafka charts into this repo and then fixed the launch script

--- launch_script.sh
+++ launch_script.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-oc login
+oc login --username=licit --password=licit
+
+oc project promenade
 
 cd mongodb || exit
 
 helm delete mongodb-replicaset
 
 helm install mongodb-replicaset .
+
+cd ..
+
+cd activemq-artemis/activemq-artemis || exit
+
+helm delete artemis
+
+helm install artemis .
+
+cd ../..
+
+cd cp-kafka-helm-charts || exit
+
+./kafka_launch_script.sh
